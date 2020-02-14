ffad5e12a2171c86072953b6799d7ea258d50de3
somnoynadno <somnoynadno@yandex.ru>
2020-02-14 12:18:03 +0300

    Current time added

--- static/js/index.js
+++ static/js/index.js
@@ -24,7 +24,7 @@ class Row extends React.Component {
 class Farm extends React.Component {
     render() {
         return (
-            <div className="container-md">
+            <div className="mt-2">
                 <Row />
                 <Row />
                 <Row />
@@ -37,12 +37,10 @@ class Farm extends React.Component {
 class Menu extends React.Component {
     render() {
         return (
-            <div className="container mt-2">
-                <div className="row align-items-center justify-content-md-center">
-                    <Title />
-                    <Speed />
-                    <Money />
-                </div>
+            <div className="row align-items-center justify-content-md-center">
+                <Title />
+                <Speed />
+                <Money />
             </div>
         )
     }
@@ -62,7 +60,7 @@ class Title extends React.Component {
 class Speed extends React.Component {
     constructor(props) {
         super(props);
-        this.speed = 1;
+        this.state = {speed : 1};
     }
 
     render() {
@@ -71,7 +69,7 @@ class Speed extends React.Component {
                   <h4>Speed:</h4>
                   <div className="btn-group" role="group">
                     <button type="button" className="btn btn-secondary">&laquo;</button>
-                    <button type="button" className="btn btn-secondary">{this.speed + 'x'}</button>
+                    <button type="button" className="btn btn-secondary">{this.state.speed + 'x'}</button>
                     <button type="button" className="btn btn-secondary">&raquo;</button>
                   </div>
               </div>
@@ -82,24 +80,63 @@ class Speed extends React.Component {
 class Money extends React.Component {
     constructor(props) {
         super(props);
-        this.money = 100;
+        this.state = {money : 100};
     }
 
     render() {
         return (
             <div className="col-4 text-center">
-                <h2> Money: {this.money} </h2>
+                <h2> Money: {this.state.money} </h2>
             </div>
         )
     }
 }
 
-ReactDOM.render(
-    <Menu />,
-    document.getElementById('menu-container')
-);
+class CurrentTime extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {time : 0};
+    }
+
+    tick() {
+        this.setState(state => ({
+            time: state.time + 1
+        }));
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => this.tick(), 1000);
+    }
+    
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }    
+
+    render() {
+        return (
+            <div className="row mt-2">
+                <div className="col">
+                    <h2 className="float-right"> Current time: {this.state.time + 's'} </h2>
+                </div>
+            </div>
+        )
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="container-md">
+                <Menu />
+                <Farm />
+                <CurrentTime />
+            </div>
+        )
+    }
+}
+
 
 ReactDOM.render(
-    <Farm />,
-    document.getElementById('farm-container')
+    <Game />,
+    document.getElementById('container')
 );
